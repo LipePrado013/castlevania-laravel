83858ace5d30762e2d86af3fe2fef4402ef57322
LipePrado <lipe.praado@gmail.com>
2022-10-29 17:22:18 -0300

    fiz um anuncio lateal com o carousel

--- resources/views/castlevania.blade.php
+++ resources/views/castlevania.blade.php
@@ -2,33 +2,112 @@
 @section('content')
 
 
-    <!-- conteudo carousel -->
-    <div class="row justify-content-center ">
-        <div class='col-auto d-none d-md-block margin'>
-            <!-- culuna central -->
-            <div id="carouselExampleFade " class="carousel slide carousel-fade" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item ">
-                        <img width="900px" class="d-block " src="img/conteudo1.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="carousel-item active">
-                        <img width="900px" class="d-block" src="img/conteudo2.jpg" alt="Segundo Slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img width="900px" class="d-block" src="img/conteudo3.jpg" alt="Terceiro Slide">
-                    </div>
+  <!-- conteudo carousel -->
+  <div class="row justify-content-center ">
+    <div class='col-auto d-none d-md-block mb-4 margin'>
+      <!-- culuna central -->
+      <div class="quadro p-2 ">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img width="900px" class="d-block " src="img/conteudo1.jpg" alt="Primeiro Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- texto da imagem 1-->
+                <div class='quadro1'>
+                  <h5>Castlevania: Nocturne | Netflix anuncia animação focada em Richter Belmont.</h5>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                </a>
+              </div><!-- texto da imagem 1 fim-->
             </div>
+            <div class="carousel-item">
+              <img width="900px" class="d-block " src="img/conteudo2.jpg" alt="Segundo Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- texto da imagem 2-->
+                <div class='quadro1'>
+                  <h5>Dracula Vlad Tepes.</h5>
+                  <p>O verdadeiro nome é Adrian Farenheit Tepes.</p>
+                </div>
+              </div><!-- texto da imagem 2 fim-->
+            </div>
+            <div class="carousel-item">
+              <img width="900px" class="d-block " src="img/conteudo3.jpg" alt="Terceiro Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- texto da imagem 3-->
+                <div class='quadro1'>
+                  <h5>Servos do dracula.</h5>
+                  <p>Isaac e Hector.</p>
+                </div>
+              </div><!-- texto da imagem 3 fim -->
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+          </a>
         </div>
+      </div>
     </div>
+  </div>
+
+  {{-- conteudo2 --}}
+
+  <div class="row conteudo2 justify-content-center">
+    <div class="col-md-4 mt-2 mb-2 imgc2">
+      <img class="mt-2" width="100%" src="img/conteudo2.jpg">
+      <br><br>
+      <p>
+        texto
+      </p>
+      <a href="">link</a>
+    </div>
+
+    <div class="col-md-4 mt-2 mb-2  ml-2 mr-2 imgc2">
+      <img class="mt-2" width="100%" src="img/conteudo3.jpg">
+      <br><br>
+      <p>
+        texto
+      </p>
+      <a href="">link</a>
+    </div>
+    <div class="col-md-2 mt-2 mb-2 d-none d-md-block bg-light">
+
+      <div class="row justify-content-center ">
+        <div class='col-auto  margin'>
+          <!-- culuna central -->
+          <div id="carousel2" class="carousel slide carousel-fade" data-ride="carousel">
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <a href="https://unisanta.br/Graduacao/Odontologia"><img width="200px" height="300px" class="d-block "
+                    src="img/anuncio1.jpeg" alt="Primeiro Slide"></a>
+              </div>
+              <div class="carousel-item">
+                <a href="https://unisanta.br/Graduacao/Odontologia"> <img width="200px" height="300px" class="d-block "
+                    src="img/anuncio2.jpeg" alt="Segundo Slide"></a>
+              </div>
+              <div class="carousel-item">
+                <a href="https://unisanta.br/Graduacao/Odontologia"><img width="200px" height="300px" class="d-block "
+                    src="img/anuncia3.jpg" alt="Terceiro Slide"></a>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel2" role="button" data-slide="prev">
+              <span class="" aria-hidden="true"></span>
+              <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel2" role="button" data-slide="next">
+              <span class="" aria-hidden="true"></span>
+              <span class="sr-only">Próximo</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {{-- mais um conteudo3  https://getcssscan.com/css-box-shadow-examples --}}
 
 
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,64 +2,64 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- css bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Castlevania</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- css bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Castlevania</title>
 </head>
 
 <body>
 
-    <div class="container-fluid">
-        <!-- inicio container -->
-        <!-- nav -->
-        <nav class='navbar navbar-expand-lg navbar-dark bg-dark ifundo'>
+  <!-- inicio container -->
+  <!-- nav -->
 
-            <div class="">
-                <a href="/">
-                    <img width='300vw' src="img/logo.png">
-                </a>
-            </div>
+  <nav class='navbar navbar-expand-lg navbar-dark bg-dark ifundo'>
 
-            <!-- menu Hamburguer -->
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!--  -->
-            <div class="collapse navbar-collapse " id="nav-target">
-                <ul class='nav  esquerda nave'>
-                    <li class='nav-item '>
-                        <a class='nav-link' href="/">Inicio</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' href="/jogo">Jogos</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' href="">Descrição</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <div class="logo">
+      <a href="/">
+        <img width='300vw' src="img/logo.png">
+      </a>
+    </div>
 
-        @yield('content')
+    <!-- menu Hamburguer -->
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!--  -->
+    <div class="collapse navbar-collapse " id="nav-target">
+      <ul class='nav  esquerda nave ml-auto'>
+        <li class='nav-item '>
+          <a class='nav-link' href="/">Inicio</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/jogo">Jogos</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="">Descrição</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-    </div> <!-- fim container -->
+  <div class="container-fluid">
 
+    @yield('content')
 
+  </div> <!-- fim container -->
 
-    <!-- js bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-    </script>
+  {{-- rodape --}}
+
+  <footer class="rodape">
+
+  </footer>
+
+  <!-- js bootstrap -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 </body>
 
