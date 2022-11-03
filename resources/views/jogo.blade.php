b6a119941596f5088a0b55207edfafd40ad239c9
LipePrado <lipe.praado@gmail.com>
2022-11-03 13:18:24 -0300

    coloquei as img nas colunas da pagina do jogos

--- resources/views/jogo.blade.php
+++ resources/views/jogo.blade.php
@@ -2,69 +2,87 @@
 
 @section('content')
   <br>
-  <div class="bg-danger p-2 ">
+  <div class="bg-danger p-3 ">
     <div class="row justify-content-center">
-      <div class="col-md-3 bg-success">
-        <img>
-        <p>1. Castlevania: Lament Of Innocence (1094)</p>
+      <div class="col-md-4 bg-success">
+        <img width="100%" src="img/jogo1.jpeg">
+        <h4>1. Castlevania: Lament Of Innocence (1094)</h4>
+        <p>Este jogo da cronologia segue os passos de Leon, o primeiro representante dos Belmont – a famosa família de
+          caça vampiros. Na história, a amada de Leon é raptada por um vampiro chamado Walter Bernhard. Para resgatá-la,
+          Belmont consegue a ajuda de Renaldo que lhe oferece o Whip Of Alchemy (ou Chicote da Alquimia). </p>
       </div>
-      <div class="col-md-3 bg-info">
-        <img>
-        <p>2. Castlevania III: Dracula’s Curse (1476)</p>
+      <div class="col-md-4 bg-info">
+        <img width="100%" src="img/jogo2.jpg">
+        <h4>2. Castlevania III: Dracula’s Curse (1476)</h4>
+        <p>Drácula, já extremamente poderoso, casou-se e teve um filho chamado Adrian. Mas como o príncipe das trevas não
+          foi feito para ser feliz, sua esposa é morta pela humanidade. Drácula, enfurecido, declara guerra à Terra.</p>
       </div>
-      <div class="col-md-3 bg-warning">
-        <img>
-        <p>3. Castlevania: Curse Of Darkness (1497)</p>
+      <div class="col-md-4 bg-warning">
+        <img width="100%" src="img/jogo3.jpg">
+        <h4>3. Castlevania: Curse Of Darkness (1497)</h4>
+        <p>O protagonista agora não é um dos Belmont, mas sim um ex-servo de Drácula: Hector, que traiu seu antigo mestre
+          – derrotado em Castlevania III. Cansado das atrocidades que testemunhou enquanto trabalhava para o lorde das
+          trevas, Hector decidiu viver uma vida normal (ou ao menos tentar).</p>
       </div>
     </div>
     <br>
     <div class="row justify-content-center">
-      <div class="col-md-3 bg-success">
-        <img>
-        <p>4. Castlevania: The Adventure (1576)</p>
+      <div class="col-md-4 bg-success">
+        <img width="100%" src="img/jogo4.jpg">
+        <h4>4. Castlevania: The Adventure (1576)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-info">
-        <img>
-        <p>5. Castlevania II: Belmont’s Revenge (1591)</p>
+      <div class="col-md-4 bg-info">
+        <img width="100%" src="img/jogo5.jpg">
+        <h4>5. Castlevania II: Belmont’s Revenge (1591)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-warning">
-        <img>
-        <p>6. Castlevania (1691)</p>
+      <div class="col-md-4 bg-warning">
+        <img width="100%" src="img/jogo6.jpeg">
+        <h4>6. Castlevania (1691)</h4>
+        <p></p>
       </div>
     </div><br>
     <div class="row justify-content-center">
-      <div class="col-md-3 bg-success">
-        <img>
-        <p>7. Castlevania II: Simon’s Quest (1698)</p>
+      <div class="col-md-4 bg-success">
+        <img width="100%" src="img/jogo7.jpg">
+        <h4>7. Castlevania II: Simon’s Quest (1698)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-info">
-        <img>
-        <p>8. Castlevania: Rondo Of Blood (1792)</p>
+      <div class="col-md-4 bg-info">
+        <img width="100%" src="img/jogo8.jpg">
+        <h4>8. Castlevania: Rondo Of Blood (1792)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-warning">
-        <img>
-        <p>9. Castlevania: Symphony Of The Night (1797)</p>
+      <div class="col-md-4 bg-warning">
+        <img width="100%" src="img/jogo9.jpg">
+        <h4>9. Castlevania: Symphony Of The Night (1797)</h4>
+        <p></p>
       </div>
     </div>
     <br>
     <div class="row justify-content-center">
-      <div class="col-md-3 bg-success">
-        <img>
-        <p>10. Castlevania: Bloodlines (1914)</p>
+      <div class="col-md-4 bg-success">
+        <img width="100%" src="img/jogo10.jpg">
+        <h4>10. Castlevania: Bloodlines (1914)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-info">
-        <img>
-        <p>11. Castlevania: Portrait Of Ruin (1944)</p>
+      <div class="col-md-4 bg-info">
+        <img width="100%" src="img/jogo11.jpg">
+        <h4>11. Castlevania: Portrait Of Ruin (1944)</h4>
+        <p></p>
       </div>
-      <div class="col-md-3 bg-warning">
-        <img>
-        <p>12. Castlevania: Aria Of Sorrow (2035)</p>
+      <div class="col-md-4 bg-warning">
+        <img width="100%" src="img/jogo12.jpg">
+        <h4>12. Castlevania: Ariawid Of Sorrow (2045)</h4>
+        <p></p>
       </div>
     </div><br>
     <div class="row justify-content-center">
-      <div class="col-md-3 bg-success">
-        <img>
-        <p>13. Castlevania: Dawn Of Sorrow (2036)</p>
+      <div class="col-md-4 bg-success">
+        <img width="100%" src="img/jogo13.jpg">
+        <h4>13. Castlevania: Dawn Of Sorrow (2036)</h4>
+        <p></p>
       </div>
     </div>
   </div>
