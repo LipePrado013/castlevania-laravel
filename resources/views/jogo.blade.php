44f11986a4cbe24568103c3d40ce1766b5720571
LipePrado <lipe.praado@gmail.com>
2022-11-02 23:59:18 -0300

    comecei a fazer a pagina do jogos

--- resources/views/jogo.blade.php
+++ resources/views/jogo.blade.php
@@ -1,7 +1,71 @@
 @extends('layouts.master')
 
 @section('content')
-
-oi isso é novo 
-
+  <br>
+  <div class="bg-danger p-2 ">
+    <div class="row justify-content-center">
+      <div class="col-md-3 bg-success">
+        <img>
+        <p>1. Castlevania: Lament Of Innocence (1094)</p>
+      </div>
+      <div class="col-md-3 bg-info">
+        <img>
+        <p>2. Castlevania III: Dracula’s Curse (1476)</p>
+      </div>
+      <div class="col-md-3 bg-warning">
+        <img>
+        <p>3. Castlevania: Curse Of Darkness (1497)</p>
+      </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+      <div class="col-md-3 bg-success">
+        <img>
+        <p>4. Castlevania: The Adventure (1576)</p>
+      </div>
+      <div class="col-md-3 bg-info">
+        <img>
+        <p>5. Castlevania II: Belmont’s Revenge (1591)</p>
+      </div>
+      <div class="col-md-3 bg-warning">
+        <img>
+        <p>6. Castlevania (1691)</p>
+      </div>
+    </div><br>
+    <div class="row justify-content-center">
+      <div class="col-md-3 bg-success">
+        <img>
+        <p>7. Castlevania II: Simon’s Quest (1698)</p>
+      </div>
+      <div class="col-md-3 bg-info">
+        <img>
+        <p>8. Castlevania: Rondo Of Blood (1792)</p>
+      </div>
+      <div class="col-md-3 bg-warning">
+        <img>
+        <p>9. Castlevania: Symphony Of The Night (1797)</p>
+      </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+      <div class="col-md-3 bg-success">
+        <img>
+        <p>10. Castlevania: Bloodlines (1914)</p>
+      </div>
+      <div class="col-md-3 bg-info">
+        <img>
+        <p>11. Castlevania: Portrait Of Ruin (1944)</p>
+      </div>
+      <div class="col-md-3 bg-warning">
+        <img>
+        <p>12. Castlevania: Aria Of Sorrow (2035)</p>
+      </div>
+    </div><br>
+    <div class="row justify-content-center">
+      <div class="col-md-3 bg-success">
+        <img>
+        <p>13. Castlevania: Dawn Of Sorrow (2036)</p>
+      </div>
+    </div>
+  </div>
 @stop
